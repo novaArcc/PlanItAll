63e27206bc659f5057e7cd629ff17d4b3a4e3135
anyagardner <anya@blisslandpizza.com>
2026-01-17 17:01:34 +0100

    refactoring

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { ScrollView } from "react-native";
-import OneFolder from "../universal-components/one-folder/one-folder";
-import tabStyles from "../universal-styles/styles-tabs";
-import Header from "../universal-components/header/header";
-import AssessmentsModal from "../pages/assessments/assessments-modal/assessments-modal";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState } from "react";
+import AssessmentsModal from "@/components/assessments-components/assessments-modal/assessments-modal";
+import OneFolder from "@/components/universal-components/one-folder/one-folder";
+import tabStyles from "@/components/universal-styles/styles-tabs";
+import Header from "@/components/universal-components/header/header";
 
 
 // assessments page
---
+++ app/recipes/one-recipe.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const oneRecipe = () => {
+  return (
+    <View>
+      <Text>This is the one recipe page</Text>
+    </View>
+  )
+}
+
+export default oneRecipe
